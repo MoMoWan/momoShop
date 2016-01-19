5f1921b737cba76c91c37e5795772b519d32d54c
MoMoWan <momo@www.gz.10086.cn>
2016-01-19 22:57:57 +0800

    Initial Commit

---
+++ momoShop/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  momoShop
+//
+//  Created by 默默 on 16/1/19.
+//  Copyright © 2016年 MoMo IOS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
